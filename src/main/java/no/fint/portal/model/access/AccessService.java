90b53fdb8cf1bf8097eeb6a5d8e780a19072d9ff
Frode <frode@sjovatsen.net>
2020-06-23 13:23:05 +0200

    fix access package / client integrety

--- src/main/java/no/fint/portal/model/access/AccessService.java
+++ src/main/java/no/fint/portal/model/access/AccessService.java
@@ -2,22 +2,26 @@ package no.fint.portal.model.access;
 
 import no.fint.portal.ldap.LdapService;
 import no.fint.portal.model.client.Client;
+import no.fint.portal.model.client.ClientService;
 import no.fint.portal.model.organisation.Organisation;
 import no.fint.portal.utilities.LdapConstants;
 import org.springframework.ldap.support.LdapNameBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class AccessService {
 
     private final LdapService ldapService;
     private final AccessObjectService accessObjectService;
+    private final ClientService clientService;
 
-    public AccessService(LdapService ldapService, AccessObjectService accessObjectService) {
+    public AccessService(LdapService ldapService, AccessObjectService accessObjectService, ClientService clientService) {
         this.ldapService = ldapService;
         this.accessObjectService = accessObjectService;
+        this.clientService = clientService;
     }
 
     public List<AccessPackage> getAccesses(String orgName) {
@@ -26,17 +30,50 @@ public class AccessService {
 
     public boolean addAccess(AccessPackage accessPackage, Organisation organisation) {
         accessPackage.setDn(
-                LdapNameBuilder.newInstance(organisation.getDn())
-                .add(LdapConstants.OU, "access")
-                .add(LdapConstants.OU, accessPackage.getName())
-                .build()
+                LdapNameBuilder.newInstance(Objects.requireNonNull(organisation.getDn()))
+                        .add(LdapConstants.OU, "access")
+                        .add(LdapConstants.OU, accessPackage.getName())
+                        .build()
         );
 
         return ldapService.createEntry(accessPackage);
     }
 
     public boolean updateAccess(AccessPackage accessPackage) {
+
+        unlinkOldClients(accessPackage, getAccessByDn(accessPackage.getDn()).getClients());
+        linkNewClients(accessPackage);
+
         return ldapService.updateEntry(accessPackage);
+    }
+
+    private void linkNewClients(AccessPackage accessPackage) {
+        accessPackage.getClients().forEach(clientDn -> clientService.getClientByDn(clientDn).ifPresent(client ->
+        {
+            client.setAccessPackage(accessPackage.getDn());
+            clientService.updateClient(client);
+        }));
+    }
+
+    private void unlinkOldClients(AccessPackage accessPackage, List<String> oldClients) {
+        oldClients.stream()
+                .filter(c -> !accessPackage.getClients().contains(c))
+                .forEach(clientDn -> clientService.getClientByDn(clientDn).ifPresent(client -> {
+                    client.getAccessPackages().clear();
+                    clientService.updateClient(client);
+                }));
+
+        accessPackage.getClients().stream()
+                .filter(c -> !oldClients.contains(c))
+                .map(clientService::getClientByDn)
+                .forEach(client -> client.ifPresent(c -> {
+                    c.getAccessPackages().stream()
+                            .map(this::getAccessByDn)
+                            .forEach(ap -> {
+                                ap.getClients().remove(c.getDn());
+                                ldapService.updateEntry(ap);
+                            });
+                }));
     }
 
     public void removeAccess(AccessPackage accessPackage) {
@@ -44,7 +81,7 @@ public class AccessService {
     }
 
     public void linkClientToAccess(AccessPackage accessPackage, Client client) {
-        accessPackage.addClient(client.getDn());
+        accessPackage.addClient(Objects.requireNonNull(client.getDn()));
         client.setAccessPackage(accessPackage.getDn());
         ldapService.updateEntry(accessPackage);
         ldapService.updateEntry(client);
---
+++ src/test/groovy/no/fint/portal/model/access/AccessServiceSpec.groovy
@@ -0,0 +1,59 @@
+package no.fint.portal.model.access
+
+import no.fint.portal.ldap.LdapService
+import no.fint.portal.model.asset.AssetService
+import no.fint.portal.model.client.Client
+import no.fint.portal.model.client.ClientObjectService
+import no.fint.portal.model.client.ClientService
+import no.fint.portal.oauth.NamOAuthClientService
+import spock.lang.Specification
+
+class AccessServiceSpec extends Specification {
+
+    private clientService
+    private ldapService
+    private clientObjectService
+    private oauthService
+    private assetService
+    private accessService
+    private accessObjectService
+
+    def setup() {
+        def organisationBase = "ou=org,o=fint"
+
+        ldapService = Mock(LdapService)
+        oauthService = Mock(NamOAuthClientService)
+        assetService = Mock(AssetService)
+        clientObjectService = new ClientObjectService(organisationBase: organisationBase)
+        clientService = new ClientService(
+                clientObjectService: clientObjectService,
+                ldapService: ldapService,
+                namOAuthClientService: oauthService,
+                assetService: assetService
+        )
+        accessObjectService = new AccessObjectService(organisationBase)
+        accessService = new AccessService(
+                ldapService,
+                accessObjectService,
+                clientService
+        )
+    }
+
+    def "Unlink old clients"() {
+        when:
+        accessService.unlinkOldClients(new AccessPackage(clients: ["a", "c"]), ["a", "b", "d"])
+
+        then:
+        ldapService.getEntry(_ as String, Client.class) >> new Client() >> new Client()
+        2 * ldapService.updateEntry(_ as Client)
+    }
+
+    def "Link new clients"() {
+        when:
+        accessService.linkNewClients(new AccessPackage(clients: ["a", "c"]))
+
+        then:
+        ldapService.getEntry(_ as String, Client.class) >> new Client() >> new Client()
+        2 * ldapService.updateEntry(_ as Client)
+    }
+}
